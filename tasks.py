b944f0249b328309efcb013fce3502fb290ee36d
Ари <arianna2811@yandex.ru>
2025-07-23 00:02:18 +0300

    fix task1

--- tasks.py
+++ tasks.py
@@ -3,31 +3,34 @@
 
 def max_count_numbers(array: list[int]) -> int:
 
-  if not isinstance(array, list):  raise TypeError
+    if not isinstance(array, list):  raise TypeError
 
-  counter = 0
-  new_array = []
-  
-  for num in array:
-    for number in array:
-        if num == number:
-            counter += 1
-    new_array.append(counter)
+    new_array = []
 
-  max_count = 0
+    for num in array:
+        counter = 0
+        for number in array:
+            if num == number:
+                counter += 1
+        new_array.append(counter)
+
+    max_count = 0
+
+    for i in range(len(new_array)):
+        if new_array[i] > max_count:
+            max_count = new_array[i]
+
+    list_max_count_elem = []
+
+    for i in range(len(new_array)):
+        if new_array[i] == max_count:
+            list_max_count_elem.append(array[i])
+
+
+    min_elem = list_max_count_elem[0]
   
-  for i in range(new_array):
-      if new_array[i] > max_count:
-         max_count = num
+    for elem in list_max_count_elem:
+        if elem < min_elem:
+            min_elem = elem
   
-  for i in range(new_elem):
-      if new_elem[i] == max_count:
-          list_max_count_elem.append(array[i])
-  
-  min_elem = list_max_count_elem [0]
-  
-  for elem in list_max_count_elem:
-      if elem < min_elem:
-          min_elem = elem
-  
-  return min_elem
+    return min_elem
