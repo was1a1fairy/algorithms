e3e89c43f923c1555567a9b2f5799395e0a7f340
Ари <arianna2811@yandex.ru>
2025-08-18 13:16:00 +0300

    fix: add annotations

--- hw4_list.py
+++ hw4_list.py
@@ -1,4 +1,12 @@
 def realloc(memory, old_size):
+    """
+    увеличение массива с элементами добавлением элементов None,
+    которых в два раза меньше чем было элементов до.
+
+    :param memory: list[elem](self.__memory)
+    :param old_size: int(self.__count)
+    :return: list[elem,elem,None]
+    """
     new_memory = malloc(old_size + old_size//2)
 
     for i in range(old_size):
@@ -8,6 +16,12 @@ def realloc(memory, old_size):
 
 
 def malloc(count):
+    """
+    выделение памяти для массива
+
+    :param count: int
+    :return: list([None,None,None...]
+    """
     return [None] * count
 
 
@@ -21,6 +35,9 @@ class List:
 
 
     def add(self, elem):
+        """
+        append(добавление элемента в конец массива)
+        """
 
         if self.__size == self.__count:
             self.__memory = realloc(self.__memory, self.__size)
@@ -30,6 +47,11 @@ class List:
 
 
     def remove(self, elem):
+        """
+        удаление элемента из массива
+        (все непустые элементы после него сдвигаются на 1 влево,
+        чтобы не было пустого элемента посередине)
+        """
 
         itarget = -1
 
@@ -47,6 +69,10 @@ class List:
 
 
     def insert(self, index, elem):
+        """
+        вставляет элемент по индексу
+        (все элементы после соответственно сдвигаются вправо)
+        """
 
         assert index < self.__count or index > 0, ValueError
 
@@ -59,6 +85,11 @@ class List:
         self.__memory[index] = elem
 
     def pop(self, index):
+        """
+        удаляет элемент по индексу,
+        сдвигает последующие элементы,
+        чтобы не образовывался пустой элемент
+        """
 
         assert index < self.__count or index > 0, ValueError
 
@@ -70,13 +101,25 @@ class List:
 
 
     def is_empty(self):
+        """
+        если массив пустой -> true, иначе -> false
+        :return: bool
+        """
         return self.__count == 0
 
 
     def clear(self):
+        """
+        очищает массив(путем выделения новой памяти стандартного размера,
+        заданного в конструкторе)
+        """
         self.__memory = malloc(4)
 
     def find(self, elem):
+        """
+        ищет индекс заданного элемента
+        :return: int(index or -1 if not elem in list)
+        """
 
         for i in range(self.__count):
             if self.__memory[i] == elem:
@@ -85,6 +128,11 @@ class List:
         return -1
 
     def count_elem(self, elem):
+        """
+        ищет количество элементов в массиве
+        :param elem: искомый элемент
+        :return: int(count elems)
+        """
 
         c = 0
         for i in range(self.__count):
